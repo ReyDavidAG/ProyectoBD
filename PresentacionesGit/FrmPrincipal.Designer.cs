316a1d0499c0454b0f183bd38651093c8651d3ad
Rey David <reydavidaguiladiaz@gmail.com>
2021-09-08 00:30:46 -0500

    Main

--- PresentacionesGit/FrmPrincipal.Designer.cs
+++ PresentacionesGit/FrmPrincipal.Designer.cs
@@ -29,19 +29,173 @@ namespace PresentacionesGit
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DTG = new System.Windows.Forms.DataGridView();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.BtnEditar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtDescripcion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtPrecio = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DTG)).BeginInit();
             this.SuspendLayout();
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Location = new System.Drawing.Point(475, 424);
+            this.BtnAgregar.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(125, 32);
+            this.BtnAgregar.TabIndex = 0;
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // TxtID
+            // 
+            this.TxtID.Location = new System.Drawing.Point(135, 49);
+            this.TxtID.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(306, 26);
+            this.TxtID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IdProducto";
+            // 
+            // DTG
+            // 
+            this.DTG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DTG.Location = new System.Drawing.Point(475, 17);
+            this.DTG.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.DTG.Name = "DTG";
+            this.DTG.Size = new System.Drawing.Size(550, 379);
+            this.DTG.TabIndex = 3;
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(621, 424);
+            this.BtnEliminar.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(125, 32);
+            this.BtnEliminar.TabIndex = 4;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // BtnEditar
+            // 
+            this.BtnEditar.Location = new System.Drawing.Point(765, 424);
+            this.BtnEditar.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.BtnEditar.Name = "BtnEditar";
+            this.BtnEditar.Size = new System.Drawing.Size(125, 32);
+            this.BtnEditar.TabIndex = 5;
+            this.BtnEditar.Text = "Editar";
+            this.BtnEditar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nombre";
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(135, 105);
+            this.TxtNombre.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(306, 26);
+            this.TxtNombre.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 172);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Descripcion";
+            // 
+            // TxtDescripcion
+            // 
+            this.TxtDescripcion.Location = new System.Drawing.Point(135, 169);
+            this.TxtDescripcion.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.TxtDescripcion.Multiline = true;
+            this.TxtDescripcion.Name = "TxtDescripcion";
+            this.TxtDescripcion.Size = new System.Drawing.Size(306, 136);
+            this.TxtDescripcion.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 333);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 18);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Precio";
+            // 
+            // TxtPrecio
+            // 
+            this.TxtPrecio.Location = new System.Drawing.Point(135, 330);
+            this.TxtPrecio.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.TxtPrecio.Name = "TxtPrecio";
+            this.TxtPrecio.Size = new System.Drawing.Size(306, 26);
+            this.TxtPrecio.TabIndex = 11;
             // 
             // FrmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(623, 479);
+            this.ClientSize = new System.Drawing.Size(1057, 484);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtPrecio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtDescripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.BtnEditar);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.DTG);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtID);
+            this.Controls.Add(this.BtnAgregar);
+            this.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "FrmPrincipal";
             this.Text = "FrmPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.DTG)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DTG;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Button BtnEditar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtDescripcion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtPrecio;
     }
 }
